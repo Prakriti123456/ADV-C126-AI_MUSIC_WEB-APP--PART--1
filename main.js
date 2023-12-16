8fa0f33d932f1d2bfadcd40de40ba9fadfb1d66b
Prakriti123456 <93470873+Prakriti123456@users.noreply.github.com>
2023-12-16 19:39:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
 scoreleftW = 0;
+scorerightW = 0;
 song1_status = "";
 song2_status = "";
 
@@ -36,12 +37,21 @@ function draw() {
 
     song1_status = song1.isPlaying();
     song2_status = song2.isPlaying();
-    if (scoreleftWrist > 0.2) {
+    if (scoreleftW > 0.2) {
         circle(leftWristX, leftWristY, 20);
+        song2.stop();
+        if (song1_status == false) {
+            song1.play();
+            document.getElementById("song").innerHTML = "song1";
+
+        }
+    }
+    if (scorerightW > 0.2) {
+        circle(rightWristX, rightWristY, 20);
         song1.stop();
         if (song2_status == false) {
             song2.play();
-            document.getElementById("song").innerHTML = "song2"
+            document.getElementById("song").innerHTML = "song2";
 
         }
     }
@@ -53,9 +63,10 @@ function play() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
-
+        scorerightW = results[0].pose.keypoints[10].score;
         scoreleftW = results[0].pose.keypoints[9].score;
         console.log("score left Wrist = " + scoreleftW);
+        console.log("score Right Wrist = " + scorerightW);
 
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
